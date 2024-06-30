ed730ce7793db02454f1dfb70d7a8a1bebe6eb23
Aman Kumar <125811177+Nama8178@users.noreply.github.com>
2024-06-30 19:44:59 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
+                                Welcome to Mega Blog !!
                             </h1>
                         </div>
                     </div>
